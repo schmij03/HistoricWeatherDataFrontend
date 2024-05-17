383f7dabdb72675ff29e7927ff03ceee006be5af
schmij03 <96172389+schmij03@users.noreply.github.com>
2024-05-17 23:12:02 +0200

    updates

--- website/nodejs/backend/backend.js
+++ website/nodejs/backend/backend.js
@@ -31,41 +31,47 @@ async function getWeatherData(type, locationValue, dateFrom, dateTill) {
     const tillDate = new Date(dateTill);
     const filter = type == 1 ? 'Region' : 'Ort';
 
-    const matchStage = {
-      '$match': { [filter]: locationValue, 'Zeit': { '$gte': fromDate, '$lte': tillDate } }
-    };
-
-    const groupStage = {
-      $group: {
-        _id: "$Zeit",
-        average_temperature: { $avg: "$Temperatur" },
-        average_dew_point: { $avg: "$Taupunkt" },
-        average_weather_condition: { $first: "$Wetterbedingung" },
-        average_wind_direction: { $avg: "$Windrichtung" },
-        average_humidity: { $avg: "$Luftfeuchtigkeit" },
-        average_precipitation: { $avg: "$Niederschlagsmenge (letzte Stunde)" },
-        average_snowfall: { $avg: "$Schneefallmenge (letzte Stunde)" },
-        average_wind_speed: { $avg: "$Windgeschwindigkeit" },
-        average_wind_gust: { $avg: "$Windböen" },
-        average_pressure: { $avg: "$Luftdruck" },
-        average_solar_radiation: { $avg: "$Sonneneinstrahlungsdauer" },
-        average_global_radiation: { $avg: "$Globalstrahlung" },
-        average_pressure_sea_level: { $avg: "$Luftdruck reduziert auf Meeresniveau" },
-        average_pressure_sea_level_standard: { $avg: "$Luftdruck reduziert auf Meeresniveau mit Standardatmosphäre" },
-        average_geopotential_height_850hPa: { $avg: "$Geopotentielle Höhe der 850 hPa-Fläche" },
-        average_geopotential_height_700hPa: { $avg: "$Geopotentielle Höhe der 700 hPa-Fläche" },
-        average_wind_direction_vector: { $avg: "$Windrichtung vektoriell" },
-        average_wind_speed_tower: { $avg: "$Windgeschwindigkeit Turm" },
-        average_wind_gust_tower: { $avg: "$Böenspitze Turm" },
-        average_temperature_instrument1: { $avg: "$Lufttemperatur Instrument 1" },
-        average_humidity_tower: { $avg: "$Relative Luftfeuchtigkeit Turm" },
-        average_dew_point_tower: { $avg: "$Taupunkt Turm" },
-        average_foehn_index: { $avg: "$Föhnindex" }
-      }
-    };
-
-    const sortStage = { $sort: { _id: 1 } };
-    const agg = [matchStage, groupStage, sortStage];
+    const agg = [
+      {
+        $match: {
+          [filter]: locationValue,
+          'Zeit': {
+            '$gte': new Date(fromDate),
+            '$lte': new Date(tillDate)
+          }
+        }
+      },  // Filter documents by the specified region or location
+      { $project: { _id: 0 } },  // Project relevant fields
+      {
+        $group: {
+          _id: "$Zeit",  // Group by time
+          average_temperature: { $avg: "$Temperatur" },  // Average temperature
+          average_dew_point: { $avg: "$Taupunkt" },  // Average dew point
+          average_weather_condition: { $first: "$Wetterbedingung" },  // Weather condition
+          average_wind_direction: { $avg: "$Windrichtung" },  // Average wind direction
+          average_humidity: { $avg: "$Luftfeuchtigkeit" },  // Average humidity
+          average_precipitation: { $avg: "$Niederschlagsmenge (letzte Stunde)" },  // Average precipitation
+          average_snowfall: { $avg: "$Schneefallmenge (letzte Stunde)" },  // Average snowfall
+          average_wind_speed: { $avg: "$Windgeschwindigkeit" },  // Average wind speed
+          average_wind_gust: { $avg: "$Windböen" },  // Average wind gust
+          average_pressure: { $avg: "$Luftdruck" },  // Average pressure
+          average_solar_radiation: { $avg: "$Sonneneinstrahlungsdauer" },  // Average solar radiation
+          average_global_radiation: { $avg: "$Globalstrahlung" },  // Average global radiation
+          average_pressure_sea_level: { $avg: "$Luftdruck reduziert auf Meeresniveau" },  // Average pressure sea level
+          average_pressure_sea_level_standard: { $avg: "$Luftdruck reduziert auf Meeresniveau mit Standardatmosphäre" },  // Average pressure sea level with standard atmosphere
+          average_geopotential_height_850hPa: { $avg: "$Geopotentielle Höhe der 850 hPa-Fläche" },  // Average geopotential height 850 hPa
+          average_geopotential_height_700hPa: { $avg: "$Geopotentielle Höhe der 700 hPa-Fläche" },  // Average geopotential height 700 hPa
+          average_wind_direction_vector: { $avg: "$Windrichtung vektoriell" },  // Average wind direction vector
+          average_wind_speed_tower: { $avg: "$Windgeschwindigkeit Turm" },  // Average wind speed tower
+          average_wind_gust_tower: { $avg: "$Böenspitze Turm" },  // Average wind gust tower
+          average_temperature_instrument1: { $avg: "$Lufttemperatur Instrument 1" },  // Average temperature instrument 1
+          average_humidity_tower: { $avg: "$Relative Luftfeuchtigkeit Turm" },  // Average humidity tower
+          average_dew_point_tower: { $avg: "$Taupunkt Turm" },  // Average dew point tower
+          average_foehn_index: { $avg: "$Föhnindex" }  // Average foehn index
+        }
+      },
+      { $sort: { _id: 1 } }  // Sort by time
+    ];    
     const coll = client.db('BA').collection('WeatherData');
     const cursor = coll.aggregate(agg);
     const result = await cursor.toArray();
