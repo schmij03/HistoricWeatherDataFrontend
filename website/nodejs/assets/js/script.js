c3df1a39b563600ed8be3f348442661edcd20364
schmij03 <96172389+schmij03@users.noreply.github.com>
2024-05-18 12:19:14 +0200

    updates

--- website/nodejs/assets/js/script.js
+++ website/nodejs/assets/js/script.js
@@ -1,82 +1,49 @@
-var af = [""];
 window.onload = function () {
   const currentUrl = window.location.href.split("/");
   var loc = currentUrl[currentUrl.length - 1];
+  var inputId = loc === "regions" ? "regionInput" : "stationInput";
+  var getDataButtonId = loc === "regions" ? "getDataRegions" : "getDataStations";
+  var endpoint = loc === "regions" ? './backend/getRegions' : './backend/getStations';
+  var autocompleteData = [];
 
-  if (loc == "regions") {
-    $.get('./backend/getRegions', function (data) {
-      for (var i = 0; i < data.length; i++) {
-        af.push(String(data[i]["_id"]));
-      }
-    });
-    autocomplete(document.getElementById("regionInput"), af);
-  } else if (loc == "stations") {
-    $.get('./backend/getStations', function (data) {
-      for (var i = 0; i < data.length; i++) {
-        af.push(String(data[i]["_id"]));
-      }
-    });
-    autocomplete(document.getElementById("stationInput"), af);
-  }
-}
-
-$(document).on("click", "#getDataRegions", function () {
-  var regionInput = $("#regionInput").val();
-  var dateFrom = $("#dateFrom").val();
-  var dateTill = $("#dateTill").val();
-  var df = new Date(dateFrom);
-  var dt = new Date(dateTill);
-  var today = new Date();
-
-  if (df > dt) {
-    alert("Das Startdatum darf nicht größer als das Enddatum sein.");
-    return;
-  }
-  if (dt > today) {
-    alert("Das Enddatum darf nicht in der Zukunft liegen.");
-    return;
-  }
-
-  $.get('./backend/getRegionData?region=' + regionInput + '&dateFrom=' + df + '&dateTill=' + dt, function (response) {
-    console.log('Received data:', response);
-    if (response && response.length > 0) {
-      const columns = Object.keys(response[0]);
-      buildTable(columns, response);
-      buildGraph(response);
-    } else {
-      console.error('Invalid response format:', response);
+  $.get(endpoint, function (data) {
+    for (var i = 0; i < data.length; i++) {
+      autocompleteData.push(String(data[i]["_id"]));
     }
+    autocomplete(document.getElementById(inputId), autocompleteData);
   });
-});
 
-$(document).on("click", "#getDataStations", function () {
-  var stationInput = $("#stationInput").val();
-  var dateFrom = $("#dateFrom").val();
-  var dateTill = $("#dateTill").val();
-  var df = new Date(dateFrom);
-  var dt = new Date(dateTill);
-  var today = new Date();
+  $(document).on("click", "#" + getDataButtonId, function () {
+    var locationInput = $("#" + inputId).val();
+    var dateFrom = $("#dateFrom").val();
+    var dateTill = $("#dateTill").val();
+    var type = loc === "regions" ? "region" : "station";
 
-  if (df > dt) {
-    alert("Das Startdatum darf nicht größer als das Enddatum sein.");
-    return;
-  }
-  if (dt > today) {
-    alert("Das Enddatum darf nicht in der Zukunft liegen.");
-    return;
-  }
+    console.log("Sending request with:", type, locationInput, dateFrom, dateTill); // Log the request parameters
 
-  $.get('./backend/getStationsData?ort=' + stationInput + '&dateFrom=' + df + '&dateTill=' + dt, function (response) {
-    console.log('Received data:', response);
-    if (response && response.length > 0) {
-      const columns = Object.keys(response[0]);
-      buildTable(columns, response);
-      buildGraph(response);
-    } else {
-      console.error('Invalid response format:', response);
+    if (new Date(dateFrom) > new Date(dateTill)) {
+      alert("Das Startdatum darf nicht größer als das Enddatum sein.");
+      return;
     }
+    if (new Date(dateTill) > new Date()) {
+      alert("Das Enddatum darf nicht in der Zukunft liegen.");
+      return;
+    }
+
+    $.get(`./backend/getData?type=${type}&location=${locationInput}&dateFrom=${dateFrom}&dateTill=${dateTill}`, function (response) {
+      console.log('Received data:', response); // Log the response
+      if (response && response.length > 0) {
+        const columns = Object.keys(response[0]);
+        buildTable(columns, response);
+        buildGraph(response);
+      } else {
+        console.error('Invalid response format:', response);
+      }
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.error("Request failed: " + textStatus + ", " + errorThrown); // Log any request failures
+    });
   });
-});
+};
 
 function buildTable(columns, data) {
   var tableHeader = "<tr>";
@@ -93,9 +60,6 @@ function buildTable(columns, data) {
     });
     tableBody += "</tr>";
   });
-
-  console.log('Table Header:', tableHeader);
-  console.log('Table Body:', tableBody);
 
   $("thead").html(tableHeader);
   $("tbody").html(tableBody);
@@ -126,8 +90,8 @@ function buildGraph(data) {
 }
 
 function autocomplete(inp, arr) {
-  var currentFocus;
-  inp.addEventListener("input", function (e) {
+  let currentFocus;
+  inp.addEventListener("input", function(e) {
     var a, b, i, val = this.value;
     closeAllLists();
     if (!val) { return false; }
@@ -136,13 +100,14 @@ function autocomplete(inp, arr) {
     a.setAttribute("id", this.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
     this.parentNode.appendChild(a);
+
     for (i = 0; i < arr.length; i++) {
-      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+      if (arr[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
         b = document.createElement("DIV");
         b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
         b.innerHTML += arr[i].substr(val.length);
         b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        b.addEventListener("click", function (e) {
+        b.addEventListener("click", function(e) {
           inp.value = this.getElementsByTagName("input")[0].value;
           closeAllLists();
         });
@@ -150,16 +115,17 @@ function autocomplete(inp, arr) {
       }
     }
   });
-  inp.addEventListener("keydown", function (e) {
+
+  inp.addEventListener("keydown", function(e) {
     var x = document.getElementById(this.id + "autocomplete-list");
     if (x) x = x.getElementsByTagName("div");
-    if (e.keyCode == 40) {
+    if (e.keyCode === 40) {
       currentFocus++;
       addActive(x);
-    } else if (e.keyCode == 38) {
+    } else if (e.keyCode === 38) {
       currentFocus--;
       addActive(x);
-    } else if (e.keyCode == 13) {
+    } else if (e.keyCode === 13) {
       e.preventDefault();
       if (currentFocus > -1) {
         if (x) x[currentFocus].click();
@@ -167,30 +133,30 @@ function autocomplete(inp, arr) {
     }
   });
 
-  function addActive(x) {
-    if (!x) return false;
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-
-  function removeActive(x) {
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-
-  function closeAllLists(elmnt) {
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-  }
-
-  document.addEventListener("click", function (e) {
+  document.addEventListener("click", function(e) {
     closeAllLists(e.target);
   });
+}
+
+function addActive(x) {
+  if (!x) return false;
+  removeActive(x);
+  if (currentFocus >= x.length) currentFocus = 0;
+  if (currentFocus < 0) currentFocus = x.length - 1;
+  x[currentFocus].classList.add("autocomplete-active");
+}
+
+function removeActive(x) {
+  for (var i = 0; i < x.length; i++) {
+    x[i].classList.remove("autocomplete-active");
+  }
+}
+
+function closeAllLists(elmnt) {
+  var x = document.getElementsByClassName("autocomplete-items");
+  for (var i = 0; i < x.length; i++) {
+    if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
 }
--- website/nodejs/backend/backend.js
+++ website/nodejs/backend/backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const router = express.Router();
 
 const uri = "mongodb+srv://weather:erhwLLotcHI8PTxj@weatherdata.zebph6n.mongodb.net/?retryWrites=true&w=majority&appName=WeatherData";
 const client = new MongoClient(uri, {
@@ -11,134 +12,71 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function getRegions() {
-  const agg = [{ '$group': { '_id': '$Region' } }];
-  const coll = client.db('BA').collection('WeatherData');
-  const cursor = coll.aggregate(agg);
-  return await cursor.toArray();
-}
-
-async function getStations() {
-  const agg = [{ '$group': { '_id': '$Ort' } }];
-  const coll = client.db('BA').collection('WeatherData');
-  const cursor = coll.aggregate(agg);
-  return await cursor.toArray();
-}
-
 async function getWeatherData(type, locationValue, dateFrom, dateTill) {
   try {
     const fromDate = new Date(dateFrom);
     const tillDate = new Date(dateTill);
-    const filter = type == 1 ? 'Region' : 'Ort';
+    const filter = type === 'region' ? 'Region' : 'Ort';
 
     const agg = [
       {
-        $match: {
+        '$match': {
           [filter]: locationValue,
           'Zeit': {
-            '$gte': new Date(fromDate),
-            '$lte': new Date(tillDate)
+            '$gte': fromDate,
+            '$lte': tillDate
           }
         }
-      },  // Filter documents by the specified region or location
-      { $project: { _id: 0 } },  // Project relevant fields
+      },
+      { '$project': { '_id': 0 } },
       {
-        $group: {
-          _id: "$Zeit",  // Group by time
-          average_temperature: { $avg: "$Temperatur" },  // Average temperature
-          average_dew_point: { $avg: "$Taupunkt" },  // Average dew point
-          average_weather_condition: { $first: "$Wetterbedingung" },  // Weather condition
-          average_wind_direction: { $avg: "$Windrichtung" },  // Average wind direction
-          average_humidity: { $avg: "$Luftfeuchtigkeit" },  // Average humidity
-          average_precipitation: { $avg: "$Niederschlagsmenge (letzte Stunde)" },  // Average precipitation
-          average_snowfall: { $avg: "$Schneefallmenge (letzte Stunde)" },  // Average snowfall
-          average_wind_speed: { $avg: "$Windgeschwindigkeit" },  // Average wind speed
-          average_wind_gust: { $avg: "$Windböen" },  // Average wind gust
-          average_pressure: { $avg: "$Luftdruck" },  // Average pressure
-          average_solar_radiation: { $avg: "$Sonneneinstrahlungsdauer" },  // Average solar radiation
-          average_global_radiation: { $avg: "$Globalstrahlung" },  // Average global radiation
-          average_pressure_sea_level: { $avg: "$Luftdruck reduziert auf Meeresniveau" },  // Average pressure sea level
-          average_pressure_sea_level_standard: { $avg: "$Luftdruck reduziert auf Meeresniveau mit Standardatmosphäre" },  // Average pressure sea level with standard atmosphere
-          average_geopotential_height_850hPa: { $avg: "$Geopotentielle Höhe der 850 hPa-Fläche" },  // Average geopotential height 850 hPa
-          average_geopotential_height_700hPa: { $avg: "$Geopotentielle Höhe der 700 hPa-Fläche" },  // Average geopotential height 700 hPa
-          average_wind_direction_vector: { $avg: "$Windrichtung vektoriell" },  // Average wind direction vector
-          average_wind_speed_tower: { $avg: "$Windgeschwindigkeit Turm" },  // Average wind speed tower
-          average_wind_gust_tower: { $avg: "$Böenspitze Turm" },  // Average wind gust tower
-          average_temperature_instrument1: { $avg: "$Lufttemperatur Instrument 1" },  // Average temperature instrument 1
-          average_humidity_tower: { $avg: "$Relative Luftfeuchtigkeit Turm" },  // Average humidity tower
-          average_dew_point_tower: { $avg: "$Taupunkt Turm" },  // Average dew point tower
-          average_foehn_index: { $avg: "$Föhnindex" }  // Average foehn index
+        '$group': {
+          '_id': "$Zeit",
+          average_temperature: { '$avg': "$Temperatur" },
+          average_dew_point: { '$avg': "$Taupunkt" },
+          average_weather_condition: { $first: "$Wetterbedingung" },
+          average_wind_direction: { '$avg': "$Windrichtung" },
+          average_humidity: { '$avg': "$Luftfeuchtigkeit" },
+          average_precipitation: { '$avg': "$Niederschlagsmenge (letzte Stunde)" },
+          average_snowfall: { '$avg': "$Schneefallmenge (letzte Stunde)" },
+          average_wind_speed: { '$avg': "$Windgeschwindigkeit" },
+          average_wind_gust: { '$avg': "$Windböen" },
+          average_pressure: { '$avg': "$Luftdruck" },
+          average_solar_radiation: { '$avg': "$Sonneneinstrahlungsdauer" },
+          average_global_radiation: { '$avg': "$Globalstrahlung" },
+          average_pressure_sea_level: { '$avg': "$Luftdruck reduziert auf Meeresniveau" },
+          average_pressure_sea_level_standard: { '$avg': "$Luftdruck reduziert auf Meeresniveau mit Standardatmosphäre" },
+          average_geopotential_height_850hPa: { '$avg': "$Geopotentielle Höhe der 850 hPa-Fläche" },
+          average_geopotential_height_700hPa: { '$avg': "$Geopotentielle Höhe der 700 hPa-Fläche" },
+          average_wind_direction_vector: { '$avg': "$Windrichtung vektoriell" },
+          average_wind_speed_tower: { '$avg': "$Windgeschwindigkeit Turm" },
+          average_wind_gust_tower: { '$avg': "$Böenspitze Turm" },
+          average_temperature_instrument1: { '$avg': "$Lufttemperatur Instrument 1" },
+          average_humidity_tower: { '$avg': "$Relative Luftfeuchtigkeit Turm" },
+          average_dew_point_tower: { '$avg': "$Taupunkt Turm" },
+          average_foehn_index: { '$avg': "$Föhnindex" }
         }
       },
-      { $sort: { _id: 1 } }  // Sort by time
-    ];    
+      { '$sort': { '_id': 1 } }
+    ];
     const coll = client.db('BA').collection('WeatherData');
     const cursor = coll.aggregate(agg);
-    const result = await cursor.toArray();
+    const result = await cursor.toArray();   
 
-    console.log('Weather data result:', result);
-
-    const columns = [
-      "Zeit",
-      "average_temperature",
-      "average_dew_point",
-      "average_weather_condition",
-      "average_wind_direction",
-      "average_humidity",
-      "average_precipitation",
-      "average_snowfall",
-      "average_wind_speed",
-      "average_wind_gust",
-      "average_pressure",
-      "average_solar_radiation",
-      "average_global_radiation",
-      "average_pressure_sea_level",
-      "average_pressure_sea_level_standard",
-      "average_geopotential_height_850hPa",
-      "average_geopotential_height_700hPa",
-      "average_wind_direction_vector",
-      "average_wind_speed_tower",
-      "average_wind_gust_tower",
-      "average_temperature_instrument1",
-      "average_humidity_tower",
-      "average_dew_point_tower",
-      "average_foehn_index"
-    ];
-
-    return { columns: columns, data: result };
+    return result;
   } catch (error) {
     console.error('Error fetching weather data:', error);
     throw error;
   }
 }
 
-router.get('/getRegions', (req, res) => {
-  getRegions().then(result => {
+router.get('/getData', (req, res) => {
+  const { type, location, dateFrom, dateTill } = req.query;
+  getWeatherData(type, location, dateFrom, dateTill).then(result => {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(result));
-  });
-});
-
-router.get('/getStations', (req, res) => {
-  getStations().then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result));
-  });
-});
-
-router.get('/getRegionData', (req, res) => {
-  const { region, dateFrom, dateTill } = req.query;
-  getWeatherData(1, region, dateFrom, dateTill).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result));
-  });
-});
-
-router.get('/getStationsData', (req, res) => {
-  const { ort, dateFrom, dateTill } = req.query;
-  getWeatherData(2, ort, dateFrom, dateTill).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result));
+  }).catch(error => {
+    res.status(500).send('Error fetching data');
   });
 });
 
